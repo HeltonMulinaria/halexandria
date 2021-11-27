85d0c8ce58ef13a5b0682e4d60282ac5b0b07a6c
HeltonMulinaria <heltonmulinaria@gmail.com>
2021-11-27 10:20:19 -0300

    Ajuste

--- src/components/produto/Pedidos.jsx
+++ src/components/produto/Pedidos.jsx
@@ -1,43 +1,56 @@
-import {useState , useEffect} from "react";
+import { useState, useEffect } from "react";
 import Api from "../api/Api";
-import ItemPedido from "./ItemPedido";
 import MenuTopo from "../menu/MenuTopo";
+import { Card } from "react-bootstrap";
+import "./pedidoStyle.css";
 
 function Pedidos() {
-    const [pedidos , setPedidos] = useState([]);
+  const [pedidos, setPedidos] = useState([]);
 
-    useEffect(() => {
-        Api.get("/pedido").then(listar);
+  useEffect(() => {
+    Api.get("/pedido").then(listar);
 
-        function listar (response) {
-           setPedidos(response.data);
-            
-        }
-    });
+    function listar(response) {
+      setPedidos(response.data);
+    }
+  });
 
-    return (
-        <>
-         <MenuTopo></MenuTopo>
+  return (
+    <>
+      <MenuTopo></MenuTopo>
+      <div className="container">
         {pedidos.map((pedido) => (
+          <Card style={{ width: "18rem", margin: "10px" }}>
+            <div key={pedido.id}></div>
+            <Card.Body>
+              <Card.Title>
+                <p>
+                  {" "}
+                  <strong>{pedido.nomeCliente}</strong>{" "}
+                </p>
+              </Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">
+                {" "}
+                <p>
+                  <strong>Número do pedido:</strong> {pedido.id}
+                </p>
+              </Card.Subtitle>
+              <Card.Text>
+                <p>
+                  <strong>Data do pedido:</strong> {pedido.dataPedido}
+                </p>
 
-            <div key = {pedido.id}>
-           
-            Data do pedido: {pedido.dataPedido}<br/>
-            Nome do cliente: {pedido.nomeCliente}<br/>
-            Status do Pedido: {pedido.pedidoStatus}<br/>
-               <ItemPedido id = {pedido.id} />        
-            <p/>
-
-            </div>
-
-
-
-        )
-
-         )}
-        </>
-
-    );
+                <p>
+                  {" "}
+                  <strong>Status do Pedido:</strong> {pedido.pedidoStatus}
+                </p>
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        ))}
+      </div>
+    </>
+  );
 }
 
 export default Pedidos;
